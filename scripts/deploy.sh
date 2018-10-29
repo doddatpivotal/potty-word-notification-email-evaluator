02d31303283f0a3a3f1852f017a08c1706c4858d
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-10-28 20:24:21 -0400

    Updates script to pass in arg

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,10 @@
 
 riff service delete potty-word-notification-email-evaluator
 
+set localpath = ${1:-.}
+
 riff function create java potty-word-notification-email-evaluator \
-    --local-path . \
+    --local-path $localpath \
     --image $DOCKER_ID/potty-word-notification-email-evaluator \
     --env FUNCTIONS_POTTY_WORD_NOTIFICATION_EMAIL_EVALUATOR_FROM=$FUNCTIONS_POTTY_WORD_NOTIFICATION_EMAIL_EVALUATOR_FROM \
     --env FUNCTIONS_POTTY_WORD_NOTIFICATION_EMAIL_EVALUATOR_TO=$FUNCTIONS_POTTY_WORD_NOTIFICATION_EMAIL_EVALUATOR_TO \
